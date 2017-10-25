c49640b725fd0c2efb0dd093319864a5331f7731
lorena salinas <nena_sr83@hotmail.com>
2017-10-24 20:09:51 -0400

    Inicio proyecto

---
+++ src/proyecto1lorena/Proyecto1Lorena.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1lorena;
+
+/**
+ *
+ * @author LAB15
+ */
+public class Proyecto1Lorena {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo, soy Lorena!!");
+    }
+    
+}
